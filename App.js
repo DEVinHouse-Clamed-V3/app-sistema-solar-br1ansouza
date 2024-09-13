c31e187b4f29aaec3ee9b962dcda1956ff94b341
Brian de Souza <briandesouza1597@gmail.com>
2024-09-13 11:13:26 -0300

    Atualizando o App.js

--- App.js
+++ App.js
@@ -1,20 +1,154 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image, StatusBar, TouchableOpacity, ScrollView } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+
+import { posts } from './posts'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.safe}>
+      <StatusBar style="auto" backgroundColor='#F56040' />
+      <View style={styles.header} >
+        <Image
+          style={styles.logo}
+          source={require('./assets/social-media-marketing.png')}
+        />
+        <View >
+          <Image
+            source={require('./assets/cattalk.png')}
+            style={{ width: 140, height: 40, borderRadius: 30 }}
+          />
+        </View>
+        <MaterialCommunityIcons name='chat' size={30} color='#FFF' />
+      </View>
+
+      <ScrollView>
+
+        <View>
+          <View style={styles.headerPost} >
+            <MaterialCommunityIcons name='check-decagram' size={20} color='#2196f3' />
+            <Text style={styles.colorText}>miau@123</Text>
+            <MaterialCommunityIcons name='dots-horizontal' size={30} color='#a9a9a9' />
+          </View>
+          <Image
+            style={styles.imagemPost}
+            source={{ uri: 'https://i.redd.it/meu-gato-%C3%A9-praticamente-o-gato-do-meme-v0-n9dleoj8dgfc1.jpg?width=720&format=pjpg&auto=webp&s=426eb562eddf7898e5ba777104b7065552be8114' }}
+          />
+
+          <View style={styles.footerPost} >
+            <TouchableOpacity>
+              <MaterialCommunityIcons name='heart' size={30} color='#fd1d1d' />
+            </TouchableOpacity>
+
+            <TouchableOpacity>
+              <MaterialCommunityIcons name='chat' size={30} color='#CCC' />
+            </TouchableOpacity>
+
+            <TouchableOpacity>
+              <MaterialCommunityIcons name='share' size={30} color='#ffdc80' />
+            </TouchableOpacity>
+          </View>
+
+          <View >
+            <Text style={styles.bodyComment}>666 likes</Text>
+          </View>
+          <View>
+            <Text style={[styles.bodyComment, { fontWeight: 'bold', paddingBottom: 1 }]}>miau@123: Selfie!</Text>
+          </View>
+          <View>
+            <Text style={[styles.bodyComment, { paddingBottom: 10 }]}>
+              Ver todos os comentrários...
+            </Text>
+          </View>
+
+        </View>
+
+        <View>
+          <View style={styles.headerPost} >
+            <MaterialCommunityIcons name='check-decagram' size={20} color='#fcaf45' />
+            <Text style={styles.colorText}>garfield_1997</Text>
+            <MaterialCommunityIcons name='dots-horizontal' size={30} color='#a9a9a9' />
+          </View>
+          <Image
+            style={styles.imagemPost}
+            source={{ uri: 'https://media.tenor.com/U28V2bPGlgUAAAAe/gato-sorrindo.png' }}
+          />
+
+          <View style={styles.footerPost} >
+            <TouchableOpacity>
+              <MaterialCommunityIcons name='heart' size={30} color='#fd1d1d' />
+            </TouchableOpacity>
+
+            <TouchableOpacity>
+              <MaterialCommunityIcons name='chat' size={30} color='#CCC' />
+            </TouchableOpacity>
+
+            <TouchableOpacity>
+              <MaterialCommunityIcons name='share' size={30} color='#ffdc80' />
+            </TouchableOpacity>
+          </View>
+
+          <View >
+            <Text style={styles.bodyComment}>420 likes</Text>
+          </View>
+          <View>
+            <Text style={[styles.bodyComment, { fontWeight: 'bold', paddingBottom: 1 }]}>garfiel_1997: Um gato XD</Text>
+          </View>
+          <View>
+            <Text style={[styles.bodyComment, { paddingBottom: 10 }]}>
+              Ver todos os comentrários...
+            </Text>
+          </View>
+
+
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  safe: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#1d1d1d'
+  },
+  logo: {
+    width: 40,
+    height: 40,
+  },
+  header: {
+    flexDirection: 'row',
+    padding: 10,
+    justifyContent: 'space-between',
     alignItems: 'center',
-    justifyContent: 'center',
+    borderBottomColor: '#a9a9a9',
+    borderBottomWidth: 1,
+
+  },
+  headerPost: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 10,
+
+  },
+  imagemPost: {
+    width: '94%',
+    height: 330,
+    borderRadius: 10,
+    alignSelf: 'center',
+  },
+  footerPost: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 5
+  },
+  colorText: {
+    color: '#CCC',
+    fontSize: 16
+  },
+  bodyComment: {
+    paddingHorizontal: 6,
+    color: '#CCC',
+    fontSize: 12
   },
 });
---
+++ posts.js
@@ -0,0 +1,45 @@
+export const posts = [
+    {
+        id: '7',
+        user: 'miau@123',
+        image: 'https://i.redd.it/meu-gato-%C3%A9-praticamente-o-gato-do-meme-v0-n9dleoj8dgfc1.jpg?width=720&format=pjpg&auto=webp&s=426eb562eddf7898e5ba777104b7065552be8114',
+        likes: 666,
+        caption: 'Selfie!',
+    },
+    {
+        id: '1',
+        user: 'garfield_1997',
+        image: 'https://media.tenor.com/U28V2bPGlgUAAAAe/gato-sorrindo.png',
+        likes: 420,
+        caption: 'Nada como viajar 12 horas de avião para apreciar um café igual ao de casa.',
+    },
+    {
+        id: '2',
+        user: 'catman.btw',
+        image: 'https://preview.redd.it/ghost-mode-batman-mode-v0-0atlezd7tgod1.jpeg?width=1080&crop=smart&auto=webp&s=badfa1d3fe854a3a26dafde4f78c9bc775df6302',
+        likes: 69,
+        caption: 'Iam Batman',
+    },
+
+    {
+        id: '4',
+        user: 'user4',
+        image: 'https://blog.cobasi.com.br/wp-content/webpc-passthru.php?src=https://blog.cobasi.com.br/wp-content/uploads/2022/12/cachorro-dormir-na-cama-capa.png&nocache=1',
+        likes: 350,
+        caption: 'A melhor parte da viagem? A volta, quando posso dormir na minha cama.',
+    },
+    {
+        id: '5',
+        user: 'user5',
+        image: 'https://viajandonajanela.com/wp-content/uploads/2020/05/camera-viagem-equipamento-fotografico.jpg',
+        likes: 145,
+        caption: 'Gastando todas as minhas economias para tirar uma foto perfeita. Valeu a pena?',
+    },
+    {
+        id: '8',
+        user: 'user8',
+        image: 'https://via.placeholder.com/300',
+        likes: 330,
+        caption: 'Explorando o mundo... desde que haja ar-condicionado.',
+    },
+]
